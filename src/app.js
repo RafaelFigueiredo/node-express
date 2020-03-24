6a87a7f9640ddd3734e7580df34461611e9f584e
Rafael Figueiredo <rafael.figueiredo91@gmail.com>
2020-03-24 00:05:39 -0300

    suport to EJS template engine added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 // EXPRESS
 const express = require('express');
+const path = require('path')
+const bodyParser = require('body-parser')
+const expressLayouts = require('express-ejs-layouts')
+
+
+
 const app = express();
-const router = express.Router();
+// const router = express.Router();
+
+
+// TEMPLATES
+app.set('view engine', 'ejs')
+app.use(expressLayouts)
+app.use(bodyParser.urlencoded())
+
+
+// STATIC FILES
+const static_path = path.join(__dirname, "../assets")
+console.log('Static files from', static_path)
+app.use("/assets", express.static(static_path) )
 
 
 // CALL ROUTERS FUNCTION 
@@ -9,4 +27,7 @@ const index = require('./routes/index');
 const fraudRoute = require('./routes/fraudRoute');
 app.use('/', index);
 app.use('/fraud', fraudRoute);
+
+
+
 module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,11 @@
 // EXPRESS
 const express = require('express');
 const router = express.Router();
-
+const expressLayouts = require('express-ejs-layouts')
 
 // SET INDEX ROUTER
 router.get('/', function (req, res, next) {
-    res.status(200).send({
-        title: "Node Express API",
-        version: "0.0.1"
-    });
+        res.render('pages/home')
 });
 
 module.exports = router;
